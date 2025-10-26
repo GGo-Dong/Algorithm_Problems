5873958c46266ce59b4e9c1523b8d8cd2963371c
GGodong <kimdongsu481@gmail.com>
2025-10-26 22:11:11 +0900

    [Silver III] Title: Histogram Sequence 2, Time: 364 ms, Memory: 51084 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21764. Histogram Sequence 2/Histogram Sequence 2.java
+++ 백준/Silver/21764. Histogram Sequence 2/Histogram Sequence 2.java
@@ -12,15 +12,23 @@ class Main {
         long[] histogram = new long[N];
         for(int i = 0; i < N; i++) histogram[i] = Long.parseLong(input[i]);
 
-        for(int j = 0; j < N-1; j++) {
-            if(histogram[j] > histogram[j+1]) {
-                histogram[j] = histogram[j+1];
-            }
+        if(N == 1) {
+            bw.write(String.valueOf(histogram[0]));
+            bw.flush();
+            bw.close();
+            return;
         }
+
+        long[] boundary = new long[N];
+        boundary[0] = histogram[0] > histogram[1] ? histogram[1] : histogram[0];
+        for(int j = 1; j < N-1; j++) {
+            boundary[j] = Math.min(Math.min(histogram[j-1], histogram[j+1]), histogram[j]);
+        }
+        boundary[N-1] = histogram[N-1] > histogram[N-2] ? histogram[N-2] : histogram[N-1];
 
         long answer = 1;
         for(int k = 0; k < N; k++) {
-            answer  = (answer * histogram[k])%MOD;
+            answer  = (answer * boundary[k])%MOD;
         }
 
         bw.write(String.valueOf(answer));
